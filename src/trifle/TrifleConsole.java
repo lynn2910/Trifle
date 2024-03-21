650d5b2ae0dd7d306b309d9d34de293fb0061c80
ccolin2 <cedric.colin@edu.univ-fcomte.fr>
2024-03-21 11:23:34 +0100

    v0.1.1 - Add a little decoration at the start of the game (Idea by @baptiste) - Add Utils.java with helper functions

--- src/trifle/TrifleConsole.java
+++ src/trifle/TrifleConsole.java
@@ -1,9 +1,24 @@
 package trifle;
 
+import trifle.boardifier.view.ConsoleColor;
 import trifle.tui.Tui;
 
+import java.io.File;
+import java.io.IOException;
+
 public class TrifleConsole {
+    private static void hello(){
+        try {
+            String kamisadoAsset = Utils.readFile(new File("src/trifle/resources/kamisado.asset.text"));
+            System.out.println("\n");
+            System.out.println(kamisadoAsset);
+        } catch (IOException e) {
+            System.out.println(ConsoleColor.RED_BOLD + "Cannot load the asset `kamisado.asset.text`." + ConsoleColor.RESET);
+        }
+    }
     public static void main(String[] args) {
+        hello();
+
         Tui tui;
         if (args.length < 1) {
             tui = new Tui();
---
+++ src/trifle/Utils.java
@@ -0,0 +1,34 @@
+package trifle;
+
+import java.io.*;
+
+/**
+ * Helper methods
+ */
+public class Utils {
+
+    /**
+     * Read a file and convert it to a String
+     * @param f The input stream to read
+     * @return The content of the file as a String
+     * @throws IOException If an error occurs while reading the file
+     */
+    public static String readFile(File f) throws IOException {
+        FileReader fr = new FileReader(f);
+        BufferedReader br = new BufferedReader(fr);
+        StringBuilder sb = new StringBuilder();
+        String line;
+        while ((line = br.readLine()) != null) {
+            sb.append(line).append("\n");
+        }
+        br.close();
+        fr.close();
+        return sb.toString();
+    }
+
+    public static void writeFile(File f, String content) throws IOException {
+        FileWriter fw = new FileWriter(f);
+        fw.write(content);
+        fw.close();
+    }
+}
