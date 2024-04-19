a1a0aebabb780ea8f23a413c3f34a2cc89632bdd
ccolin2 <cedric.colin@edu.univ-fcomte.fr>
2024-04-19 19:29:13 +0200

    Add logic to manage the user input
    - The logic loop is back again
    - Add methods when a player or a bot is playing
    - Add a help message when the user type `?`
    - Add a pattern to check if the action is correctly formed (!= if the move is good)
    - Add placeholder for the methods `botTurn` and `analyseAndPlay`

--- src/trifle/control/TrifleController.java
+++ src/trifle/control/TrifleController.java
@@ -2,11 +2,14 @@ package trifle.control;
 
 import trifle.boardifier.control.Controller;
 import trifle.boardifier.model.Model;
+import trifle.boardifier.model.Player;
+import trifle.boardifier.view.ConsoleColor;
 import trifle.boardifier.view.View;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.regex.Pattern;
 
 public class TrifleController extends Controller {
 
@@ -29,8 +32,7 @@ public class TrifleController extends Controller {
 
         this.update();
 
-        // FIXME
-        while((!this.model.isEndStage()) && false) {
+        while(!this.model.isEndStage()) {
             this.playTurn();
             this.endOfTurn();
             this.update();
@@ -52,7 +54,80 @@ public class TrifleController extends Controller {
     private void playTurn() {
         // TODO
 
-//        ActionList actions = ActionFactory.generatePutInContainer(model, pawn, "trifle_board", row, col);
+        Player p = model.getCurrentPlayer();
 
+        switch (p.getType()) {
+            case Player.HUMAN: {
+                playerTurn(p);
+                break;
+            }
+            case Player.COMPUTER: {
+                botTurn(p);
+                break;
+            }
+            default: {
+                System.out.println(ConsoleColor.RED + "Player type is nether a player or a human, what?\nThis error is critical, the game will exit." + ConsoleColor.RESET);
+                System.exit(1);
+            }
+        }
+
+//        ActionList actions = ActionFactory.generatePutInContainer(model, pawn, "trifle_board", row, col);
+    }
+
+    private static void printMoveHelp(){
+        System.out.println("\nHow to move my pawn?");
+        System.out.println("We will take an example. If you are Player 1 and want to move your cyan pawn at A1 to G7, you input will be:");
+        System.out.println("  CG7");
+        System.out.println("`C` for `cyan` and where you want to move it.");
+        System.out.println("Here is each color code:");
+        System.out.println("- Cyan:   `c`");
+        System.out.println("- Blue:   `b`");
+        System.out.println("- Purple: `p`");
+        System.out.println("- White:  `d`");
+        System.out.println("- Orange: `e`");
+        System.out.println("- Red:    `f`");
+        System.out.println("- Green:  `g`");
+        System.out.println("- Black:  `n`");
+        System.out.println("NOTE: You can also provide the pawn coordinates, like `A1G7`, but we advise you to use the color code, as it simpler to keep a track on the game.");
+        System.out.println("You can view again this message by typing `?` in your input.\n");
+    }
+
+    private static final Pattern MOVE_PATTERN = Pattern.compile("^([a-h][1-8]|[cbpdefgn])([a-h][1-8])$");
+
+    private void playerTurn(Player p) {
+        boolean ok = false;
+
+        while (!ok) {
+            System.out.print(p.getName() + " > ");
+            try {
+                String move = consoleSysIn.readLine().toUpperCase().trim();
+
+                if (move.equals("?")) {
+                    printMoveHelp();
+                    continue;
+                }
+
+                if ((move.length() != 3 && move.length() != 4) || !MOVE_PATTERN.matcher(move).find()) {
+                    System.out.println(ConsoleColor.RED + "Your input must be like `A1G7` or `cG7`, where the first is whether the color code or the coordinate of the move, and after, where you want to move it." + ConsoleColor.RESET);
+                    System.out.println(ConsoleColor.YELLOW + "The match is insensitive and doesn't differentiate upper and lower case" + ConsoleColor.RESET);
+                }
+
+                ok = this.analyseAndPlay(move);
+            } catch (IOException e) {
+                System.out.println(ConsoleColor.RED + e.getMessage());
+                e.printStackTrace();
+                System.out.println(ConsoleColor.RESET);
+                System.exit(1);
+            }
+        }
+    }
+
+    private void botTurn(Player p) {
+        // TODO computer play?
+    }
+
+    private boolean analyseAndPlay(String move) {
+        // TODO
+        return true;
     }
 }
