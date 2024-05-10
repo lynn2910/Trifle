57735f935730d06743d18eba95516838102ad0de
ccolin2 <cedric.colin@edu.univ-fcomte.fr>
2024-05-10 16:03:44 +0200

    Clean the training script

--- resources/generate_training_data.sh
+++ resources/generate_training_data.sh
@@ -13,16 +13,6 @@ games_folder="${games_folder%%/}"
 echo "This program will run Trifle games until the training data file reaches a certain size."
 echo "Finished data will be saved as 'training_data.csv' in the same folder as this script."
 
-# Compile Trifle (assuming Java compiler is installed)
-#echo "Compiling Trifle..."
-#cd ..
-#javac -d compiledTrifle "${trifle_position}"/src/**/*.java
-#
-#if [[ $? -ne 0 ]]; then
-#  echo "Error: Compilation failed!"
-#  exit 1
-#fi
-
 # Function to get file size
 get_file_size() {
   stat -c%s "$1"
@@ -37,9 +27,9 @@ game_count=0
 while [[ $(get_file_size "training_data.csv") -lt "$max_memory" ]]; do
   echo "Running game $game_count..."
 
-  # The wait time of 1s is to not burn the CPU of the NAS
+  # The wait time of 100ms is to not burn the CPU of the NAS
   WAIT_BEFORE_END=100 java -cp ../out/production/trifle trifle.TrifleConsole 2 \
-         --output-files "${games_folder}"/game_${game_count}.in \
+         --output-moves "${games_folder}/game_${game_count}.in" \
          --training-path "training_data.csv"
 
   if [ $? != 0 ]; then
@@ -51,6 +41,3 @@ done
 
 # Print results
 echo "$game_count games have been run"
-
-# Clean up compiled Trifle directory
-rm -rf compiledTrifle
